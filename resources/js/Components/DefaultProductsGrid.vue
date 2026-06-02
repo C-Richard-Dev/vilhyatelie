88c4f3bef3fbc7b9990bd0546ccb01ed0d2a2a33
C-Richard-Dev <richarddeabreu2017@gmail.com>
2026-06-02 20:00:22 -0300

    feat: update product grid layout and truncate product names

--- resources/js/Components/DefaultProductsGrid.vue
+++ resources/js/Components/DefaultProductsGrid.vue
@@ -24,7 +24,7 @@ const truncate = (text, length = 20) => {
 		<div class="mb-4 flex items-center justify-center">
 			<span class="font-bold text-pink-600">{{ title }}</span>
 		</div>
-		<div class="grid grid-cols-2 md:grid-cols-4 gap-4 px-4">
+		<div class="grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-4">
 			<Link
 				v-for="product in products"
 				:key="product.id"
@@ -37,7 +37,7 @@ const truncate = (text, length = 20) => {
 				<span v-else-if="product.status === 'latest'" class="absolute top-2 left-2 bg-blue-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow z-10">Últimos</span>
 				<img :src="product.image ? `/storage/${product.image}` : '/images/default.png'" alt="Imagem do produto" class="w-full h-48 object-cover bg-pink-50">
 				<div class="p-4 flex-1 flex flex-col">
-					<h2 class="text-lg font-semibold text-pink-600 mb-2 group-hover:text-white">{{ product.name }}</h2>
+					<h2 class="text-lg font-semibold text-pink-600 mb-2 group-hover:text-white">{{ truncate(product.name, 30) }}</h2>
 							<p class="text-gray-700 text-sm mb-4 flex-1 hidden md:block group-hover:text-white">{{ truncate(product.description, 100) }}</p>
 					<div class="mt-auto flex items-center justify-between">
 						<span class="text-xl font-bold text-pink-500 group-hover:text-white">R$ {{ product.price }}</span>
